8bd671a0f704e5b102e73e46447057fe8fcaf36a
jamcoy <james@coynemail.net>
2016-10-07 17:04:51 +0100

    Add local storage of JWT token

--- js/controller.js
+++ js/controller.js
@@ -2,10 +2,18 @@ angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
         $scope.title = "Welcome To Angular Todo!";
     })
-    .controller('RegisterController', function($scope, UserAPIService) {
+    .controller('RegisterController', function($scope, UserAPIService, store) {
 
         $scope.registrationUser = {};
-        var URL = "https://morning-castle-91468.herokuapp.com/";
+        var url = "https://morning-castle-91468.herokuapp.com/";
+
+        /*
+        var authStorage = {
+            name: "StorageTest"
+        };
+
+        store.set('obj', authStorage);
+        */
 
         $scope.login = function() {
             UserAPIService.callAPI(URL + "accounts/api-token-auth/", $scope.data).then(function(results) {
@@ -21,13 +29,21 @@ angular.module('RouteControllers', [])
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
 
-                UserAPIService.callAPI(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
+                UserAPIService.callAPI(url + "accounts/register/", $scope.registrationUser).then(function(results) {
                     $scope.data = results.data;
-                    alert("You have successfully registered to Angular Todo");
-                    $scope.login();
+                    if ($scope.data.username == $scope.registrationUser.username && $scope.data.password == $scope.registrationUser.password){
+                        alert("You have successfully registered to Angular Todo");
+
+                        UserAPIService.callAPI(url + "accounts/api-token-auth/", $scope.data).then(function(results) {
+                            $scope.token = results.data.token;
+                            store.set('username', $scope.registrationUser.username);
+                            store.set('authToken', $scope.token);
+                        }).catch(function(err) {
+                            console.log(err);
+                        });
+                    }
                 }).catch(function(err) {
-                    alert("Oops! Something went wrong!");
-                    console.log(err);
+                    console.log(err)
                 });
             }
         };
